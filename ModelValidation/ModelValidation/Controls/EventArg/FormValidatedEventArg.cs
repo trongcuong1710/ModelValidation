4ccd580d2564ea9ca691b755f5a5832bf954548b
trongcuong1710 <trongcuong1710@gmail.com>
2014-04-29 17:07:43 +0700

    Update Custom Controls

---
+++ ModelValidation/ModelValidation/Controls/EventArg/FormValidatedEventArg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelValidation.Controls.EventArg
+{
+    /// <summary>
+    /// event argument indicate when form finish validated
+    /// </summary>
+    public class FormValidatedEventArg : System.EventArgs 
+    {
+        /// <summary>
+        /// determine whether form's data is valid
+        /// </summary>
+        private bool isValid;
+
+        /// <summary>
+        /// list of validation result
+        /// not contain any item if form's data valid
+        /// </summary>
+        private List<ValidationResult> validationResult;
+
+        /// <summary>
+        /// determine whether form's data is valid
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return this.isValid;
+            }
+        }
+
+        /// <summary>
+        /// list of validation result
+        /// not contain any item if form's data valid
+        /// </summary>
+        public List<ValidationResult> ValidationResult
+        {
+            get
+            {
+                return this.validationResult;
+            }
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="isValid"></param>
+        /// <param name="validationResult"></param>
+        public FormValidatedEventArg(bool isValid, List<ValidationResult> validationResult)
+            : base()
+        {
+            try
+            {
+                this.isValid = isValid;
+                this.validationResult = validationResult;
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
+        }        
+    }
+}
---
+++ ModelValidation/ModelValidation/Controls/Form.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using ModelValidation.Controls.EventArg;
+
+namespace ModelValidation.Controls
+{
+    /// <summary>
+    /// Custom form
+    /// Support model validating
+    /// </summary>
+    public class Form : System.Windows.Forms.Form 
+    {
+        /// <summary>
+        /// model which will bind to control inside this form
+        /// </summary>
+        public object Model { get; set; }
+
+        /// <summary>
+        /// form validated
+        /// fired when form finish validated its data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public delegate void FormValidatedEventHandler(object sender, FormValidatedEventArg e);
+
+        /// <summary>
+        /// form validated event handler
+        /// </summary>
+        public event FormValidatedEventHandler Form_Validated;
+
+        /// <summary>
+        /// submit form data
+        /// </summary>
+        public void Submit()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// validating model
+        /// </summary>
+        public bool Validate()
+        {
+            try
+            {
+                //validate form model
+                List<ValidationResult> validationResult = Validator.Validate(this.Model);
+
+                bool isValid = validationResult.Count > 0;
+
+                FormValidatedEventArg arg = new FormValidatedEventArg(isValid, validationResult);
+
+                //raised form validated event handler
+                this.Form_Validated(this, arg);
+
+                if (validationResult.Count > 0)
+                    return false;
+
+                return true;
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
+        }
+    }
+}
